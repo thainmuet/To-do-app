ffce08f9115d0de02a616da8d644758dfe07334f
thainguyen <thainm.uet@gmail.com>
2021-05-23 19:12:07 +0700

    Completed project

--- init.sql
+++ init.sql
@@ -1,27 +1,20 @@
 DROP DATABASE IF EXISTS to_do;
-
 CREATE DATABASE IF NOT EXISTS to_do;
-
 USE to_do;
-
 CREATE TABLE IF NOT EXISTS user (
     username CHAR(16) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     PRIMARY KEY (username)
 );
-
 CREATE TABLE IF NOT EXISTS importance (
     flag CHAR(50),
     grade TINYINT(2),
     PRIMARY KEY (flag)
 );
-
 CREATE TABLE IF NOT EXISTS frequency (
-    frequency_id INT AUTO_INCREMENT NOT NULL,
     frequency CHAR(20) NOT NULL,
-    PRIMARY KEY (frequency_id)
+    PRIMARY KEY (frequency)
 );
-
 CREATE TABLE IF NOT EXISTS project (
     project_id INT AUTO_INCREMENT NOT NULL,
     username CHAR(16) NOT NULL,
@@ -34,12 +27,10 @@ CREATE TABLE IF NOT EXISTS project (
     flag CHAR(50) DEFAULT 'None',
     PRIMARY KEY (project_id, username),
     FOREIGN KEY (username)
-        REFERENCES user (username),
-    FOREIGN KEY (flag) 
-        REFERENCES importance (flag)
+    REFERENCES user (username),
+    FOREIGN KEY (flag)
+    REFERENCES importance (flag)
 );
-
-
 CREATE TABLE IF NOT EXISTS task (
     task_id INT AUTO_INCREMENT NOT NULL,
     project_id INT,
@@ -48,71 +39,30 @@ CREATE TABLE IF NOT EXISTS task (
     description TEXT,
     due_date DATE,
     added_date DATE,
-    frequency_id INT,
+    frequency CHAR(20) DEFAULT 'None',
     tag VARCHAR(255),
     completed TINYINT(1) DEFAULT FALSE,
     flag CHAR(50) DEFAULT 'None',
     PRIMARY KEY (task_id, username),
     FOREIGN KEY (project_id)
-        REFERENCES project (project_id),
+    REFERENCES project (project_id),
     FOREIGN KEY (username)
-        REFERENCES user (username),
-    FOREIGN KEY (frequency_id)
-        REFERENCES frequency (frequency_id),
-    FOREIGN KEY (flag) 
-        REFERENCES importance (flag)
+    REFERENCES user (username),
+    FOREIGN KEY (frequency)
+    REFERENCES frequency (frequency),
+    FOREIGN KEY (flag)
+    REFERENCES importance (flag)
 );
-
 INSERT INTO frequency(frequency)
-    VALUES
-        ('None'),
-        ('Daily'),
-        ('Weekly'),
-        ('Monthly');
-
+VALUES
+    ('None'),
+    ('Daily'),
+    ('Weekly'),
+    ('Monthly');
 INSERT INTO importance(flag, grade)
-    VALUES
-        ('None', 1 ),
-        ('No worry', 2),
-        ('Normal', 3),
-        ('Importance', 4),
-        ('Very importance', 5);
-
-INSERT INTO user(username, password)
-    VALUES
-        ('root', "root"),
-        ('admin', "admin");
-
-INSERT INTO project(title, username)
-    VALUES
-        ("DB project", "root"),
-        ("AI project", "root");
-
-INSERT INTO task(title, username, flag, completed, project_id, tag)
-    VALUES 
-        ("DB assignment", "root", "None", TRUE, 1, "assignment"),
-        ("Lab course", "root", "Normal", TRUE, 1, ""),
-        ("SE course", "root", "Importance", TRUE, 2, ""),
-        ("SE assignment", "root", "Importance", TRUE, 2, "assignment"),
-        ("AI practice", "root", "No worry", FALSE, 2, "");
-
-
--- Get all tasks from user "root"
-SELECT * FROM task WHERE username="root";
-
-
--- Get all tasks having "assignment" as tag
-SELECT * FROM task WHERE tag="assignment";
-
-
--- Get tasks from project which has ID 1 of user "root"
-SELECT * FROM task WHERE username="root" and project_id=1;
-
-
--- Get tasks having "assignment" substring in title 
-SELECT * FROM task WHERE title LIKE "%assignment%";
-
-
--- Get total grade from completed taks
-SELECT SUM(grade) as Total_grade FROM importance 
-    WHERE flag IN (SELECT flag FROM task WHERE completed=TRUE);
+VALUES
+    ('None', 1 ),
+    ('No worry', 2),
+    ('Normal', 3),
+    ('Importance', 4),
+    ('Very importance', 5);
